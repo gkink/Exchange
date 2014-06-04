9d8c2e51b2244c65b8e47c444976235123171e45
gkink <gkink12@freeuni.egu.ge>
2014-06-05 01:50:24 +0400

    ტრანზაქცია და ტრანზაქციების კონტეინერი, ტრანზაქცია დასამთავრებელია

--- Exchange/src/guestSession/Transaction.java
+++ Exchange/src/guestSession/Transaction.java
@@ -1,24 +1,114 @@
 package guestSession;
 
-public class Transaction {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import dbClasses.DBqueryGenerator;
+import dbConnection.ConnectionPool;
+
+public class Transaction implements TransactionInterface{
 	
-	public Transaction(int ID){
-		
+	private int ID;
+	private DateTime dateTime;
+	private Connection con;
+	private ConnectionPool pool;
+	private DBqueryGenerator queryGenerator;
+	private Map<String, String> transaction;
+	
+	private ResultSet itemInfo(){
+		queryGenerator = new DBqueryGenerator();
+		String query = queryGenerator.getTransactionQuery(ID);
+		try {
+			pool = ConnectionPool.getInstance();
+			con = pool.getConnectionFromPool();
+			ResultSet set = con.createStatement().executeQuery(query);
+			return set;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
-	public void getUsersFromTransaction(){
-		
+	private void closeResultSet(ResultSet set){
+		try {
+			set.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void getItemsFromTransaction(){
-		
+	private void getUserAndItemName(int itemID){
+		String query = queryGenerator.getItemChangedWithUser(itemID);
+		try {
+			ResultSet set = con.createStatement().executeQuery(query);
+			set.next();
+			set.getString(1);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void transactionTime(){
-		
+	private void getTransactionFromBases(){
+		ResultSet itemIDs = itemInfo();
+		try {
+			while(itemIDs.next()){
+				getUserAndItemName(itemIDs.getInt(1));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		closeResultSet(itemIDs);
+		pool.returnConnectionToPool(con);
 	}
 	
-	public void transactionItemsWithUsers(){
-		
+	public void createNewTransaction(){
+		//TODO
+	}
+ 	
+	public Transaction(int ID, DateTime dateTime){
+		this.ID = ID;
+		this.dateTime = dateTime;
+		getTransactionFromBases();
+	}
+	
+	public Transaction(){
+		createNewTransaction();
+	}
+
+	@Override
+	public Iterator<String> getUsers() {
+ 		return transaction.keySet().iterator();
+	}
+
+	@Override
+	public Iterator<String> getItems() {
+		return transaction.values().iterator();
+	}
+
+	@Override
+	public Map<String, String> getItemsAndUsers() {
+		return transaction;
+	}
+
+	@Override
+	public void addToTheBases() {
+		//TODO
+	}
+
+	@Override
+	public DateTime getDateTime() {
+		return dateTime;
+	}
+
+	@Override
+	public int getID() {
+		return ID;
 	}
 }
--- Exchange/src/guestSession/TransactionContainer.java
+++ Exchange/src/guestSession/TransactionContainer.java
@@ -1,10 +1,13 @@
 package guestSession;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import dbClasses.DBqueryGenerator;
-
+import dbConnection.ConnectionPool;
 
 /**
  * @author Gio
@@ -14,24 +17,69 @@ import dbClasses.DBqueryGenerator;
  */
 public class TransactionContainer {
 	
-	private List<Transaction > transactions;
+	private List<Transaction> transactions;
+	private DBqueryGenerator queryGenerator;
+	private int currentTransaction = 0;
+	private ResultSet transactionIDs;
+	
+	private ResultSet getTransactionIDSet(String query){
+		try {
+			ConnectionPool pool = ConnectionPool.getInstance();
+			Connection con = pool.getConnectionFromPool();
+			ResultSet set = con.createStatement().executeQuery(query);
+			pool.returnConnectionToPool(con);
+			return set;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private void createTransactions(){
+		try {
+			while(transactionIDs.next()){
+				DateTime dateTime = new DateTime(transactionIDs.getString(2));
+				Transaction tr = new Transaction(transactionIDs.getInt(1), dateTime);
+				transactions.add(tr);	
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		closeResultSet();
+	}
+	
+	private void closeResultSet(){
+		try {
+			transactionIDs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void init(){
+		transactions = new ArrayList<Transaction>();
+		queryGenerator = new DBqueryGenerator();
+	}
 	
 	public TransactionContainer(){
-		transactions = new ArrayList<Transaction>();
-		DBqueryGenerator queryGenerator = new DBqueryGenerator();
-		String query = queryGenerator.getTransactionsQuery();
-		
+		init();
+		transactionIDs = getTransactionIDSet(queryGenerator.getTransactionsQuery());
+		createTransactions();
 	}
 	
 	public TransactionContainer(int userID){
-		
+		init();
+		transactionIDs = getTransactionIDSet(queryGenerator.getTrasactionsForUserQuery(userID));
+		createTransactions();
 	}
 	
 	public Transaction getTransaction(int num){
-		return null;
+		return transactions.get(num);
 	}
 	
-	public Transaction getNextTransaction(int num){
-		return null;
+	public Transaction getNextTransaction(){
+		return transactions.get(currentTransaction++);
 	}
 }
