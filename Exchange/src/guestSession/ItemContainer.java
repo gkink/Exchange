d3a9bfdb73b699e637022899a87730cbac43aebd
ikoba12 <ikoba12@freeuni.edu.ge>
2014-06-12 17:46:12 +0400

    dasamtavrebelia jer

--- Exchange/src/guestSession/ItemContainer.java
+++ Exchange/src/guestSession/ItemContainer.java
@@ -1,5 +1,17 @@
 package guestSession;
 
-public class ItemContainer {
+import java.util.List;
 
+import dbClasses.DBqueryGenerator;
+import dbClasses.QueryExecutor;
+
+public class ItemContainer {
+	private List<Item> items;
+	private DBqueryGenerator generator;
+	private QueryExecutor executor;
+	public ItemContainer(DBqueryGenerator generator, QueryExecutor executor){
+		this.generator=generator;
+		this.executor=executor;
+		
+	}
 }
